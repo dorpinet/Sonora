fdf4ec28a85387f7eb51630c544266444ce5a136
Dora <dorpiness@gmail.com>
2026-05-30 03:24:23 +0300

    Fix missing Label import in PianoScreen

--- src/main/java/com/dorpine/ui/screens/PianoScreen.java
+++ src/main/java/com/dorpine/ui/screens/PianoScreen.java
@@ -8,6 +8,7 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
